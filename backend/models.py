105ac2c87ab967f81639970cd60f24e0d09e3a50
Sadhasivam S <62686520+Ishidaa-Asahi@users.noreply.github.com>
2024-10-05 16:07:59 +0530

    Blogs, Users, Followers models was created

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,29 @@
 from sqlmodel import SQLModel, Field
+from sqlalchemy import Column, Integer
+from datetime import date
 
-class BlogPost(SQLModel, table = True):
-    __tablename__ = "blogs"
-    id: int = Field(default = True, primary_key=True)
-    author: str
-    username: str
+class Blogs(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True, sa_column=Column(Integer, autoincrement=True, primary_key=True))
+    author_name: str
+    author_username: str = Field(foreign_key="users.username")
+    author_username: str
     title: str
     body: str
-    date: str
-    thumbnail: str
+    created_date: date
+    blog_cover: bytes
+
+class Users(SQLModel, table=True):
+    id: int = Field(primary_key=True, default=None, sa_column=Column(Integer, primary_key=True, autoincrement=True))
+    name: str
+    username: str = Field(unique=True, index=True)
+    email: str = Field(unique=True, index=True)
+    hashed_password: str
+    created_at: date
+    updated_at: date
+    profile_pic: bytes
+    
+class Followers(SQLModel, table=True):
+    id: int = Field(primary_key=True, default=None,sa_column=Column(Integer, primary_key=True, autoincrement=True))
+    followee_id: int = Field(foreign_key="users.id")
+    follower_id: int = Field(foreign_key="users.id")
+    followed_at: date

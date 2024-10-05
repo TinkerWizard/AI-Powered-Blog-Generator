1b1c8358020b9a5f61bf96f81552111dabbf4183
Sadhasivam S <62686520+Ishidaa-Asahi@users.noreply.github.com>
2024-10-05 16:03:47 +0530

    crud operations were added

--- backend/crud.py
+++ backend/crud.py
@@ -1,11 +1,20 @@
 from sqlmodel import Session
-from models import BlogPost
+from models import Blogs, Users, Followers
 
-def create_blog_post(db: Session, blog_post: BlogPost):
-    db.add(blog_post)
+def create_blog_post(db: Session, blog: Blogs):
+    db.add(blog)
     db.commit()
-    db.refresh(blog_post)
-    return blog_post
+    db.refresh(blog)
+    return blog
 
 def get_blog_posts(db: Session):
-    return db.query(BlogPost).all()
+    return db.query(Blogs).all()
+
+def get_all_users(db: Session):
+    return db.query(Users).all()
+
+def get_all_followers(db: Session, user_id: int):
+    return db.query(Followers).filter(Followers.followee_id == user_id).all()
+
+def get_following_blogs():
+    return None

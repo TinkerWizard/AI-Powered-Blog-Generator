d2bbb5852831f5683e28a8643858babb31da2921
Sadhasivam S <62686520+Ishidaa-Asahi@users.noreply.github.com>
2024-10-05 16:06:36 +0530

    CORS middlware was added. Few routes were added

--- backend/main.py
+++ backend/main.py
@@ -1,31 +1,42 @@
 from fastapi import FastAPI, Depends
 from sqlmodel import Session
-from database import create_db_and_tables, engine
-from models import BlogPost
-from crud import get_blog_posts
+from database import create_db_and_tables, engine, get_db
+from models import Blogs
+from crud import get_blog_posts, get_all_followers
+from fastapi.middleware.cors import CORSMiddleware
+from api.main import api_router
 
-
+from dotenv import load_dotenv
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Adjust to your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 
 
 create_db_and_tables()
 
-def get_db():
-    with Session(engine) as session:
-        yield session
-# connect database
-# SQL_MODEL
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/blogposts/", response_model=list[BlogPost])
+@app.get("/blogs/", response_model=list[Blogs])
 def read_blog_posts(db: Session = Depends(get_db)):
-    blog_posts = db.query(BlogPost).all()
-    print("Retrieved blog posts:", blog_posts)  # Print the retrieved posts
-    return blog_posts
+    return get_blog_posts(db)
+    
 
+@app.get('/followers/{user_id}')
+async def get_followers(user_id: int, db: Session = Depends(get_db)):
+    users = get_all_followers(db, user_id)
+    print(users)
+    return users
+
+app.include_router(api_router)
 
 if __name__ == "__main__":
     import uvicorn

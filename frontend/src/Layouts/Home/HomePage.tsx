ff981bbf5e8dda5d0e6caef2554e29586b21d10c
Sadhasivam S <62686520+Ishidaa-Asahi@users.noreply.github.com>
2024-10-05 16:10:41 +0530

    HomePage layout was created. This contains Blogs and recommended section

--- frontend/src/Layouts/Home/HomePage.tsx
+++ frontend/src/Layouts/Home/HomePage.tsx
@@ -2,6 +2,7 @@ import Button from '@mui/material/Button';
 import { TopBar } from '../../Utils/TopBar';
 import { Interests } from './components/Interests';
 import { BlogDisplay } from './components/BlogDisplay';
+import { UserDisplay } from './components/UserDisplay';
 import { Recommended } from './components/Recommended';
 
 export const HomePage:React.FC<{}> = () => {
@@ -11,10 +12,10 @@ export const HomePage:React.FC<{}> = () => {
             <div className="d-none d-xl-block d-xxl-block">
                 <TopBar />
                 <div className='d-flex' style={{width: '100%'}}>
-                    <div className='py-5 px-2 d-flex flex-column justify-content-center align-items-center border border-right' style={{width: '60%'}}>
-                        <Interests />
+                    <div className='py-5 px-2 d-flex flex-column justify-content-start align-items-center border border-right' style={{width: '60%'}}>
+                        {/* <Interests /> */}
                         <BlogDisplay />
-
+                        {/* <UserDisplay /> */}
                     </div>
                     <div className='d-flex justify-content-center align-items-start' style={{width: '40%'}}>
                         <Recommended />

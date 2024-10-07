eddd641ee288fad952718b53788a48072e3e9fc0
Sadhasivam S <62686520+Ishidaa-Asahi@users.noreply.github.com>
2024-10-07 15:59:01 +0530

    refactored by removing the redundant pieces of code

--- backend/api/utilities/generate_blog.py
+++ backend/api/utilities/generate_blog.py
@@ -11,17 +11,3 @@ def generate_blog_using_genai(template):
     response = model.generate_content(template)
     print(response.text)
     return response.text
-
-# topic = input("Enter a topic: ")
-# number_of_words = 1000
-# # Passages, bullet points, numbered points
-# type = "bullet points"
-# template = f"""Use the following pieces of context to build content at the end. 
-# If you cannot produce a content, just say that you can't produce a content. 
-# Keep the content within the number of characters provided. Always say something relative and positive at the end of the content. 
-# Topic: {topic}
-# Number of words: {number_of_words}
-# Type: {type}
-# Blog:"""
-
-# generate_blog(template)

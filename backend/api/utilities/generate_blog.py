ac6839418199b65b8a8a95dbc62942a33a93454b
Sadhasivam S <62686520+Ishidaa-Asahi@users.noreply.github.com>
2024-10-08 23:05:29 +0530

    blog/ content can be generated using the gemini api and the template parameter. result is split into 'title' and 'body'  and returned

--- backend/api/utilities/generate_blog.py
+++ backend/api/utilities/generate_blog.py
@@ -9,5 +9,19 @@ model = genai.GenerativeModel(model_name="gemini-1.5-flash")
 
 def generate_blog_using_genai(template):
     response = model.generate_content(template)
-    print(response.text)
-    return response.text
+    # print(response.text)
+    content = response.text
+    print(content)
+    title=""
+    body=""
+    if "Title:" in content and "Body:" in content:
+        # Extract title and body
+        title_start = content.index("Title:") + len("Title:")
+        body_start = content.index("Body:") + len("Body:")
+
+        title = content[title_start:body_start].strip()
+        body = content[body_start:].strip()
+    return {
+        "title": title,
+        "body": body
+    }

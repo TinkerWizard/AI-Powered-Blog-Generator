a6bd5a547a77492e2925b143bad8baa6e284b4dd
Sadhasivam S <62686520+Ishidaa-Asahi@users.noreply.github.com>
2024-10-11 10:29:42 +0530

    Session of sqlAlchemy was used instead of sqlmodel

--- backend/api/utilities/users.py
+++ backend/api/utilities/users.py
@@ -1,14 +1,13 @@
 from models import Users
-from sqlmodel import Session, select
+from sqlmodel import select
+from sqlalchemy.orm import Session
 from sqlalchemy.ext.asyncio import AsyncSession
 from database import get_db
 from fastapi import Depends, HTTPException
 
-async def get_author_name_using_author_username(author_username: str, db: AsyncSession):
-    statement = select(Users).where(Users.username == author_username)
-    result = await db.execute(statement)  # Use 'exec' with async session
-    user = result.scalars().first()  # Get the first result
-    return user.name if user else None
+def get_author_name_using_author_username(author_username: str, db: AsyncSession):
+    user = db.query(Users).filter(Users.username == author_username).first()  # Get the first result
+    return user.name
 
 
 def get_author_id_using_author_username(author_username: str, db: Session = Depends(get_db)):

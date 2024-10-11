4decb663869905b212471ae52916ca19a35cedbe
Sadhasivam S <62686520+Ishidaa-Asahi@users.noreply.github.com>
2024-10-11 10:36:23 +0530

    JWT token was added in the header

--- frontend/src/Layouts/Home/components/BlogDisplay.tsx
+++ frontend/src/Layouts/Home/components/BlogDisplay.tsx
@@ -1,12 +1,20 @@
 import { useEffect, useState } from 'react';
 import Blog from './../../../Models/Blog';
+import { useParams } from 'react-router-dom';
 export const BlogDisplay: React.FC<{}> = () => {
-    const username = 'rick.grimes';
-    const [blogs, setBlogs] = useState<Blog[]>();
+    const token = localStorage.getItem('token');
+    const { username } = useParams();
+    const [blogs, setBlogs] = useState<Blog[]>([]);
     useEffect(() => {
         async function fetchBlogs() {
             try {
-                const response = await fetch(`http://127.0.0.1:8000/api/blogs/following/${username}`);
+                const response = await fetch(`http://127.0.0.1:8000/api/blogs/following/${username}`, {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                        'Content-Type': 'application/json'
+                    }
+                });
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
@@ -26,27 +34,31 @@ export const BlogDisplay: React.FC<{}> = () => {
             {/* Desktop */}
             <div className="d-none d-xl-block d-xxl-block" style={{ width: '100%' }}>
                 {
-                    blogs?.map((blog) => (
-                        <div key={blog.id} className='d-flex justify-content-between align-items-center px-5 border-bottom' style={{ width: '100%' }}>
-                            {/* Content section */}
-                            <div style={{ width: '80%' }} className='p-3'>
-                                <p>{blog.author_name}</p>
-                                <h5>{blog.title}</h5>
-                                <p style={{ maxWidth: '80%' }}>
-                                    {blog.body.split(' ').slice(0, 20).join(' ')}{blog.body.split(' ').length > 20 ? '...' : ''}
-                                </p>
-
-                                <p>Published on: {new Date(blog.created_date).toLocaleDateString()}</p>
+                    blogs?.length > 0 ? (
+                        blogs?.map((blog) => (
+                            <div key={blog.id} className='d-flex justify-content-between align-items-center px-5 border-bottom' style={{ width: '100%' }}>
+                                {/* Content section */}
+                                <div style={{ width: '80%' }} className='p-3'>
+                                    <p>{blog.author_name}</p>
+                                    <h5>{blog.title}</h5>
+                                    <p style={{ maxWidth: '80%' }}>
+                                        {blog.body.split(' ').slice(0, 20).join(' ')}{blog.body.split(' ').length > 20 ? '...' : ''}
+                                    </p>
+                                    <p>Published on: {new Date(blog.created_date).toLocaleDateString()}</p>
+                                </div>
+                                {/* Thumbnail section */}
+                                <div style={{ width: '20%' }}>
+                                    {blog.blog_cover && (
+                                        <img src={`data:image/jpeg;base64,${blog.blog_cover}`} alt={blog.title} width={'100%'} />
+                                    )}
+                                </div>
                             </div>
-                            {/* Thumbnail section */}
-                            <div style={{ width: '20%' }}>
-                                {blog.blog_cover && (
-                                    <img src={`data:image/jpeg;base64,${blog.blog_cover}`} alt={blog.title} width={'100%'} />
-                                )}
-                            </div>
-                        </div>
-                    ))
+                        ))
+                    ) : (
+                        <p>Follow users or interests to see blogs</p>
+                    )
                 }
+
             </div>
             {/* Mobile */}
             <div className="d-block d-sm-none">

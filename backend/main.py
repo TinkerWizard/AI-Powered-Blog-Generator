5c9e0d11da66df4a4802de96e410ad1b6a363b9c
Sadhasivam S <62686520+Ishidaa-Asahi@users.noreply.github.com>
2024-10-11 10:30:14 +0530

    app reload was added

--- backend/main.py
+++ backend/main.py
@@ -22,4 +22,4 @@ app.include_router(api_router, prefix='/api')
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)

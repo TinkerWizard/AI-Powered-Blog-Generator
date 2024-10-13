c8b45e0cb18a60179a9342483b65d0046655cd8f
Sadhasivam S <62686520+Ishidaa-Asahi@users.noreply.github.com>
2024-10-14 00:53:12 +0530

    UserProfile route was added. App.tsx is wrapped by redux Provider and redux PersistGate

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,50 +5,28 @@ import { HomePage } from './Layouts/Home/HomePage';
 import { GenerateBlogPage } from './Layouts/Generate Blog/GenerateBlogPage';
 import { SignUpPage } from './Layouts/Authentication/SignUpPage';
 import { SignInPage } from './Layouts/Authentication/SignInPage';
+import { UserProfile } from './Layouts/Profile/UserProfile';
+import { Provider } from 'react-redux';
+import store, { persistor } from './store/store'
+import { PersistGate } from 'redux-persist/integration/react';
 function App() {
-  const [content, setContent] = useState<string>();
-  const [topic, setTopic] = useState<string>();
-  const handleTopicChange = (event: any) => {
-    event.preventDefault();
-    setTopic(event.target.value);
-  }
-  const handleSubmit = async (event: any) => {
-    // event.preventDefault();
-    // alert(topic);
-    try {
-      const response = await fetch("http://127.0.0.1:5000/generate", {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ topic })
-      });
-
-      const data = await response.json();
-      if (response.ok) {
-        setContent(data.content);
-        alert(data.content);
-      }
-      else {
-        alert("Error in generating or someting");
-      }
-
-    } catch (error: any) {
-      console.log("Error:", error);
-    }
-  }
   return (
-    <Router>
-      <Routes>
-        {/* Authentication */}
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/signin" element={<SignInPage />} />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <Routes>
+            {/* Authentication */}
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/signin" element={<SignInPage />} />
 
-        {/* Home */}
-        <Route path="/home/:username" element={<HomePage />} />
-        <Route path="/blogs/generate" element={<GenerateBlogPage />}/>
-      </Routes>
-    </Router>
+            {/* Home */}
+            <Route path="/home/:username" element={<HomePage />} />
+            <Route path="/home/user/:author_username" element={<UserProfile />} />
+            <Route path="/blogs/generate" element={<GenerateBlogPage />} />
+          </Routes>
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 }
 
